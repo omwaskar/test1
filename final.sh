701e4f9e6e554afda1255dc15c5be608de745885
Kali <kali@rhel8.example.com>
2021-02-25 01:46:53 -0500

    first commit

---
+++ final.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo "Enter Task no." 
+while read tsk
+do
+  case $tsk in
+	  1) tk1 ;;
+  esac
+done
+info(){
+	echo "Enter Your Name"
+	read name
+	echo "Enter Your Mail ID"
+	read email
+	echo "Enter Your College Name"
+	read college
+
+	echo "Name:$name" > task.txt
+	echo "Email:$email" >> task.txt
+	echo "College_Name:$college" >> task.txt
+}
+
+tk1(){
+	info
+	ls -l /rhel | grep workshop >> task.txt
+	cat /rhel/workshop >> task.txt
+	
+	if ( ls -l /rhel | grep workshop )
+	then
+		echo "You got a 10/10 in this task."
+		echo "you got a 10/10 in this task." >> task.txt
+	else
+		echo "you got a 0/10 in this task."
+		echo "you got a 0/10 in this task." >> task.txt
+	fi
+
+	echo "Welcome to rhel8-workshop" >> rhel8-workshop
+	openssl aes-256-cbc -pass file:"./rhel8-workshop" -a -salt -in ./task.txt -out ./task1.txt
+
+	rm -rf rhel8-workshop
+	rm -rf task.txt
+}
+
