d3fe6d28904bebcced7dde9ef3ac826289ff83f5
omwaskar <53070424+omwaskar@users.noreply.github.com>
2021-03-18 13:58:17 +0530

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -20,12 +20,27 @@ if [[ "172.17.$domain.20" == "$(ip a | grep -o -i 172.17.$domain.20)" ]]; then
 	   (( score += 10 ))
        else
            echo "netmask is not correct"
+	   echo "    "
+	   echo "Name: $name"
+	   echo "Email: $email"
+	   echo "College_Name: $college"
+           echo "You got $score Marks"
        fi
     else
         echo "gateway is not correct"
+	echo "    "
+	echo "Name: $name"
+	echo "Email: $email"
+	echo "College_Name: $college"
+	echo "You got $score Marks"
     fi
 else
     echo "Ip address is not correct"
+    echo "    "
+    echo "Name: $name"
+    echo "Email: $email"
+    echo "College_Name: $college"
+    echo "You got $score Marks"
 fi
 
 #2
@@ -36,32 +51,31 @@ fi
                 then
                         if(( $(cat /etc/passwd | grep sarah | grep -o -i /sbin/nologin | wc -l)==1 ))
                         then
-				echo "    "
-        			echo "Name: $name" 
-	        		echo "Email: $email" 
-			        echo "College_Name: $college"
-        	        	echo "You got a 10/10 in this task."
 				(( score += 10 )) 
 			else
 				echo "    "
 		                echo "Name: $name"
 		                echo "Email: $email"
 		                echo "College_Name: $college"
-		                echo "You got a 0/10 in this task."
+		                echo "You got $score Marks"
 			fi
 		else
         		echo "    "
 	                echo "Name: $name"
 	                echo "Email: $email"
         	        echo "College_Name: $college"
-	                echo "You got a 0/10 in this task."
+	                echo "You got $score Marks"
 	        fi
         else
 		echo "    "
         	echo "Name: $name" 
         	echo "Email: $email" 
 	        echo "College_Name: $college"
-                echo "You got a 0/10 in this task." 
+                echo "You got $score Marks" 
         fi
 
+echo "    "
+echo "Name: $name" 
+echo "Email: $email" 
+echo "College_Name: $college"
 echo "You got $score Marks"
