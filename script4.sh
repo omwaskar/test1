cd24205987964c7397ef14b315b82558c0e507b6
omwaskar <53070424+omwaskar@users.noreply.github.com>
2021-04-01 07:59:23 +0530

    Create script4.sh

---
+++ script4.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+
+echo "Enter Your System no like 0,1,2...etc."
+read domain
+echo "Enter Your Name"
+read name
+
+score=0
+score12=0
+score13=0
+score14=0
+score15=0
+score16=0
+score17=0
+score18=0
+score19=0
+
+#6
+
+if(( $(crontab -u natasha -l | grep "25 06" | grep -oi "/bin/echo Hello Test" | wc -l)==1 ))
+then
+	(( score6 += 10 ))
+else
+	(( score6 += 0 ))
+fi
+
+
+#7
+
+if(( $(ls -l /var/tmp/ | grep -o -i fstab | wc -l)==1 ))
+then
+	if(( $(ls -l /var/tmp/ | grep fstab  | grep -o -i root | wc -l)>1 ))
+	then
+		if(( $(ls -l /var/tmp/ | grep fstab | grep -o -i x | wc -l)==0 ))
+		then
+			if(( $(getfacl -a -p /var/tmp/fstab | grep -o -i natasha:rw- | wc -l)==1 ))
+        		then
+				if(( $(getfacl -a -p /var/tmp/fstab | grep -o -i harry:--- | wc -l)==1 ))
+                        	then
+					if(( $(ls -l /var/tmp/ | grep fstab | grep -o -i r | wc -l)>=6 ))
+                			then
+					        (( score7 += 10 ))
+                		        else
+                                		(( score7 += 0 ))
+                        		fi
+				else
+					(( score7 += 0 ))
+				fi
+			else
+				(( score7 += 0 ))
+			fi
+		else
+			(( score7 += 0 ))
+		fi
+	else
+		(( score7 += 0 ))
+	fi
+else
+(( score7 += 0 ))
+fi
+
+#8
+
+if(( $(cat /etc/chrony.conf | grep "server classroom.example.com iburst" | wc -l)==1 ))
+then
+        (( score8 += 10 ))
+else
+        (( score8 += 0 ))
+fi
+
+#9
+
+if (cat /etc/passwd | grep -o -i station$domain) >/dev/null
+then
+	 if(( $(cat /etc/passwd | grep station$domain | grep -oP '(?<=:x:).*(?=::)' | grep ^1088 | grep -oP '.*(?=:)' | wc -l)==1 ))
+         then
+		if(( $(cat /etc/passwd | grep station$domain | grep -o -i /sbin/nologin | wc -l)==1 ))
+                then
+			(( score9 += 10 )) 
+		else
+			(( score9 += 0 ))
+		fi
+	else
+		(( score9 += 0 ))
+        fi
+else
+	(( score9 += 0 ))
+fi
+
+#10
+
+if [[ "$(find /usr/local/ 2>/dev/null | grep ^/usr/local | wc -l)" == "$(tar -tf /my_backup.tar.gz 2>/dev/null | wc -l)" ]]; then
+	if [[ "$(tar -tf /my_backup.tar.gz 2>/dev/null | grep ^usr/local | wc -l)" == "$(tar -tf /my_backup.tar.gz 2>/dev/null | wc -l)" ]]; then
+		(( score10 += 10 ))
+	else
+		(( score10 += 0 ))
+	fi
+else
+	(( score10 += 0 ))
+fi
+
+#11
+
+if [[ "$(cat /usr/share/dict/words 2>/dev/null | wc -l)" != 0 ]]; then
+	 if [[  "$(cat /root/result.txt 2>/dev/null | wc -l)" != 0 ]]; then
+		if [[ "$(cat /usr/share/dict/words 2>/dev/null | grep ^ich | wc -l)" == "$(cat /root/result.txt 2>/dev/null | wc -l)" ]]; then
+			if [[ "$(cat /root/result.txt 2>/dev/null | grep ^ich | wc -l)" == "$(cat /root/result.txt 2>/dev/null | wc -l)" ]]; then
+				(( score11 += 10 ))
+			else
+				(( score11 += 0 ))
+			fi
+		else
+			(( score11 += 0 ))
+		fi
+	else
+		(( score11 += 0 ))
+	fi
+else
+	(( score11 += 0 ))
+fi
+
+(( score = score12 + score13 + score14 + score15 + score16 + score17 + score18 + score19 ))
+
+echo "    "
+echo "System no: $domain"
+echo "Name: $name" 
+echo "Scenario 12: $score12 Marks"
+echo "Scenario 13: $score13 Marks"
+echo "Scenario 14: $score14 Marks"
+echo "Scenario 15: $score15 Marks"
+echo "Scenario 16: $score16 Marks"
+echo "Scenario 17: $score17 Marks"
+echo "Scenario 18: $score18 Marks"
+echo "Scenario 19: $score19 Marks"
+echo "    "
+echo "Total Marks: $score"
+echo "    "
+echo "Thank you for solving industry Scenarios."
+echo "    "
